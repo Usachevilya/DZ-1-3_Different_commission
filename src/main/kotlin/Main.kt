e78c15191bfb1c0b736984f01c6918a661780853
Usachevilya <usachevilya@bk.ru>
2022-03-14 11:48:24 +0300

    Work on bugs 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,45 @@
+var commissionVisaMir = 0.75
+var commissionMasterMaestro = 0.6
+val maximumTransferAmountPerDay = 150_000_00
+val maximumTransferAmountPerMonth = 600_000_00
+val minimumCommissionVisaMir = 35_00
+val maximumTransferWithZeroCommissionMasterMaestro = 75_000_00
+val maximumTransferAmountPerDayVK = 15_000_00
+val maximumTransferAmountPerMonthVK = 40_000_00
 
-val feeVisaMir = 0.75
 
 fun main() {
-    println("Коммиссия составила: " +
-            commissionCalculation(100_000_00) +
-            " рублей")
+    println(
+        "Коммиссия составила: " +
+                commissionCalculation(100_000_00) +
+                " рублей"
+    )
+}
 
-    }
-
-fun commissionCalculation (transfer: Int, cardType: String = "VKPay", transferAmountPerMonth: Int = 0): Int {
+fun commissionCalculation(transfer: Int, cardType: String = "VKPay", transferAmountPerMonth: Int = 0): Int {
     var commission = 0
-    when(cardType) {
+    when (cardType) {
         "Master", "Maestro" -> {
-            if(transfer <= 150_000_00 && transferAmountPerMonth <= 600_000_00){
-                if (transferAmountPerMonth > 75_000_00) {
-                commission = (transfer * 0.6 / 100 +20).toInt()
+            if (transfer <= maximumTransferAmountPerDay && transferAmountPerMonth <= maximumTransferAmountPerMonth) {
+                if (transferAmountPerMonth > maximumTransferWithZeroCommissionMasterMaestro) {
+                    commission = (transfer * commissionMasterMaestro / 100 + 2000).toInt()
                 }
             } else println("Превышен лимит перевода")
         }
         "Visa", "Mir" -> {
-            if (transfer <= 150_000_00 && transferAmountPerMonth <= 600_000_00) {
-                val commisionAmount = (transfer * feeVisaMir / 100).toInt()
-                if (commisionAmount <= 35_00) {
-                    commission = 35_00/100
+            if (transfer <= maximumTransferAmountPerDay && transferAmountPerMonth <= maximumTransferAmountPerMonth) {
+                val commisionAmount = (transfer * commissionVisaMir / 100).toInt()
+                if (commisionAmount <= minimumCommissionVisaMir) {
+                    commission = minimumCommissionVisaMir
                 } else {
-                    commission = commisionAmount / 100
+                    commission = commisionAmount
                 }
             } else {
                 println("Превышен лимит перевода")
             }
         }
         "VKPay" -> {
-            if (transfer <= 15_000_00 && transferAmountPerMonth <= 40_000_00 ) {
+            if (transfer <= maximumTransferAmountPerDayVK && transferAmountPerMonth <= maximumTransferAmountPerMonthVK) {
                 commission = 0
             } else println("Превышен лимит перевода")
         }
